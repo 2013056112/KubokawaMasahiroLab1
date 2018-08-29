f5efb1e12c08fd14a8b66ffdd624da32de77a9fe
Masahiro C. Kubokawa <2013056112@ust-ics.mygbiz.com>
2018-08-29 22:14:34 +0800

    LabExercise1

---
+++ constraintlayout/src/main/java/masahiro/kubokawa/com/constraintlayout/ConstraintLayout.java
@@ -0,0 +1,13 @@
+package masahiro.kubokawa.com.constraintlayout;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ConstraintLayout extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_constraint_layout);
+    }
+}
